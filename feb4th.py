3aa927bb523f2aad38f1326269b2e6324b62bcd4
ryan-c123 <ryanc545@nycstudents.net>
2025-02-04 10:02:25 -0500

    integers or sm

---
+++ feb4th.py
@@ -0,0 +1,25 @@
+#integers represent whole numbers
+""" age: 14
+#Floats represent decimals
+wallet=5.45
+#boolean represents true or false, used in evaluations
+graduated = false 
+
+def add(x,y):
+    print(x+y)
+#input asks yhe user a question and store their responses as a value
+bill = float(input("what was the bill?")) """
+#print(type(bill))
+#add(40, bill)
+
+#lists
+students = ["Joanna", "Deivid", "David", "other David", "Ethan"]
+#similar to saying for i in range(5): print9students[i})]
+#print(students[4])
+""" for student in students:
+    print(student) """
+moneys = [1,2,3,4,5,6]
+total = 0
+for money in moneys:
+    total=total+money
+    print(total)
