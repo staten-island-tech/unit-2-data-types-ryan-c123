fd966ec2029dc0f5597c6d16ac8e7d68ff768e0e
ryan-c123 <ryanc545@nycstudents.net>
2025-02-05 10:15:57 -0500

    struggling

--- feb4th.py
+++ feb4th.py
@@ -7,19 +7,36 @@ graduated = false
 
 def add(x,y):
     print(x+y)
-#input asks yhe user a question and store their responses as a value
+#input asks the user a question and store their responses as a value
 bill = float(input("what was the bill?")) """
 #print(type(bill))
 #add(40, bill)
 
 #lists
-students = ["Joanna", "Deivid", "David", "other David", "Ethan"]
+""" students = ["Joanna", "Deivid", "David", "other David", "Ethan"]
 #similar to saying for i in range(5): print9students[i})]
 #print(students[4])
-""" for student in students:
-    print(student) """
+for student in students:
+    print(student) 
 moneys = [1,2,3,4,5,6]
 total = 0
 for money in moneys:
     total=total+money
-    print(total)
+    print(total) """
+""" values = [1,2.23,5,7,2,30,15]
+#print(values)
+ for i in values:
+    print(i) 
+print(values[0])
+print(values[6]) """
+#python
+
+""" def sentence(input):
+    print(input)
+sentence("type") """
+# Accept user input
+user_input = input("Please enter a sentence: ")
+words = user_input.split()
+print(len(words))
+
+
