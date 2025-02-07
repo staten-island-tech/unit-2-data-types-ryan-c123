e684fa0c904487716d1c7e5a763ef82f50a6c5ae
ryan-c123 <ryanc545@nycstudents.net>
2025-02-07 10:16:27 -0500

    madlibs and odd/even

--- feb4th.py
+++ feb4th.py
@@ -44,16 +44,70 @@ print(z)"""
 """ answer = input("Please enter a sentence: ")
 words = answer.split()
 print(len(words)) """
-noun_1 = input("type a noun")
-noun_2 = input("type another noun")
-verb_1 = input("type a verb")
-verb_2 = input("type another verb")
-verb_3 = input("type another verb")
-adj_1 = input("type a adjective")
-adj_2 = input("type another adjective")
-adv = input("type an adverb")
-feeling = input("type an emotion")
-celebrity = input("name a celebrity")
-number = input("type a number")
-pronoun = input("type a pronoun")
-#I was so (feeling) when my favorite person, (celeb) showed up to my (adj_1) school. They were (adj_2) when they (verb_1)  next to me! I couldn't help but (adv) (verb) with a (noun). When they left, they (verb) my school (number) (noun)s.
+#MADLIBS PROJECT
+""" noun_1 = input("type a noun: ")
+noun_2 = input("type another noun: ")
+verb_1 = input("type a verb: ")
+verb_2 = input("type another verb: ")
+verb_3 = input("type another verb: ")
+adj_1 = input("type a adjective: ")
+adj_2 = input("type another adjective: ")
+adv = input("type an adverb: ")
+feeling = input("type an emotion: ")
+celeb = input("name a celebrity: ")
+number = input("type a number: ")
+pronoun = input("type a pronoun: ")
+print(f"I was so {feeling} when my favorite person, {celeb} showed up to my {adj_1} school. They were so {adj_2} when they {verb_1}  next to me! I couldn't help but {adv} {verb_2} with a {noun_1}. When they left, they {verb_3} my school {number} {noun_2}s.")
+ """
+
+
+
+
+
+""" def login(password):
+    #if the statement is true then go to next line
+    if password == "secret":
+        print("logged in")
+    else:
+        print("password incorrect")
+#x = input("whats the password")
+#login(x)
+
+def grade(score):
+    if score >=92:
+        print("A")
+        #use elif, bc it means that if the conditions of the if above it are met it wont be ran
+    elif score >= 82:
+        print("B")
+    elif score >= 72:
+        print("C")
+    else:
+        print("F") """
+#score = int(input("what the score"))
+#grade(score)
+
+""" def gamble(age, id):
+    if age >= 21:
+        if id:
+            print("gamble away")
+        else:
+            print("You need an id")
+    else:
+        print("go away ur too young") """
+""" 
+def gamble(age, id):
+    if age >= 21 and id == true:
+        print('have fun')
+    elif age>= 21 and id== false:
+        print("you need an id")
+    else:
+        print("youre too young")
+age = 21
+id = true """
+def oe(number):
+ if (number % 2) == 0:
+    print("Even number")
+ else:
+    print ("Odd number")
+number = int(input("type a number to see if it is odd or even: "))
+oe(number)
